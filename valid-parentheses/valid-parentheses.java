ba1363cf5407d55f3f49f8bc31699527ba5d5187
Harshit Goyal <harshit_goyal@outlook.com>
2022-05-25 06:00:00 +0530

    Time: 2 ms, Memory: 41.7 MB - LeetHub

---
+++ valid-parentheses/valid-parentheses.java
@@ -0,0 +1,31 @@
+class Solution {
+
+    public boolean isValid(String expr) {
+        Deque<Character> stack = new ArrayDeque<Character>();
+        for (int i = 0; i < expr.length(); i++) {
+            char x = expr.charAt(i);
+
+            if (x == '(' || x == '[' || x == '{') {
+                stack.push(x);
+                continue;
+            }
+            if (stack.isEmpty()) return false;
+            char check;
+            switch (x) {
+                case ')':
+                    check = stack.pop();
+                    if (check == '{' || check == '[') return false;
+                    break;
+                case '}':
+                    check = stack.pop();
+                    if (check == '(' || check == '[') return false;
+                    break;
+                case ']':
+                    check = stack.pop();
+                    if (check == '(' || check == '{') return false;
+                    break;
+            }
+        }
+        return (stack.isEmpty());
+    }
+}
