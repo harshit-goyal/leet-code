5741884fe42cb1e369e45b2a9bc6e3b950532eea
Harshit Goyal <harshit_goyal@outlook.com>
2022-05-29 13:09:36 +0530

    Time: 46 ms, Memory: 43.2 MB - LeetHub

---
+++ clone-graph/clone-graph.java
@@ -0,0 +1,46 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+       if (node == null)
+            return null;
+        // assumption : this is a undirected connected graph
+        LinkedList<Node> q = new LinkedList<Node>();
+        //map contains only copied nodes
+        HashMap<Integer, Node> map = new HashMap<Integer, Node>();
+        q.add(node);// in q original nodes
+        map.put(node.val, new Node(node.val));
+        while (q.size() > 0){
+            Node now = q.pollFirst();
+            Node copied = map.get(now.val);
+            copied.neighbors = new ArrayList<Node>();
+            for (Node n:now.neighbors){
+                if (!map.containsKey(n.val)){
+                    q.add(n);
+                    map.put(n.val, new Node(n.val));
+                }
+                copied.neighbors.add(map.get(n.val));
+            }
+        }//while
+        return map.get(node.val);
+        
+    }
+}
