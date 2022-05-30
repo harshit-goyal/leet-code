c08becdfd28be46e8530d758e4f928a6b31bc757
Harshit Goyal <harshit_goyal@outlook.com>
2022-05-30 13:57:37 +0530

    Time: 8 ms, Memory: 45 MB - LeetHub

---
+++ third-maximum-number/third-maximum-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int thirdMax(int[] a) {
+        Set <Integer> maxElements = new HashSet <>();
+        for(int element : a)
+        {
+            maxElements.add(element);
+            if(maxElements.size() > 3)
+                maxElements.remove(Collections.min(maxElements));
+        }
+        if(maxElements.size() == 3)
+            return Collections.min(maxElements);
+        return Collections.max(maxElements);
+    }
+}
